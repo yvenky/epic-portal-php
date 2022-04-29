50f908af5765995c3bac4e82fb1ed30eacb69e80
BHNayan <103599059+BHNayan@users.noreply.github.com>
2022-04-30 04:45:53 +0600

    init

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,6 +2,10 @@
 
 sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
+
+/usr/bin/supervisord -c /app/docker/supervisord.confchmod 
+
+
 php-fpm -D
 
 while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
