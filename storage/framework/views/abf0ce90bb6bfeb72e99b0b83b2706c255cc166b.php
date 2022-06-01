a0ed4ffd7c2375a78a0e1550a67a4c2918bfa22f
bhuvan yerramsetty <69368938+bhuvan1410@users.noreply.github.com>
2022-06-01 14:50:54 -0500

    init

--- storage/framework/views/abf0ce90bb6bfeb72e99b0b83b2706c255cc166b.php
+++ storage/framework/views/abf0ce90bb6bfeb72e99b0b83b2706c255cc166b.php
@@ -1,19 +1,18 @@
-@extends('backend.inc.master')
-
-@section('main-content')
+<?php $__env->startSection('main-content'); ?>
 <!--// Main Area Start //-->
 <section class="panel-wrapper panel-center">
-    @if(Session::has('success-message')) 
+    <?php if(Session::has('success-message')): ?> 
     <div class="alert alert-success" role="alert">
-        {{Session::get('success-message')}}
+        <?php echo e(Session::get('success-message')); ?>
+
       </div>
-     @endif 
+     <?php endif; ?> 
     <div class="form-wrapper">
         <div class="form-heading">
             <h6>Entity New Partner Add</h6>
         </div>
-        <form action="{{url('entity-newpartner-add-submit')}}" method="POST" id="entity-newpartner-add-form">
-            @csrf
+        <form action="<?php echo e(url('entity-newpartner-add-submit')); ?>" method="POST" id="entity-newpartner-add-form">
+            <?php echo csrf_field(); ?>
 
             <div class="form-content pb-0">
                 <div class="row mt-4">
@@ -24,9 +23,9 @@
                                 <select name="INVESTOR_ID" class="form-select select-matcher-obj select2" id="investor_select-list"  data-select2-id="investor_select_list_select"  aria-label="Default select example">
                                 <optgroup label="Please Select Investor" data-select2-id="investor_select-list_select">
                                     <option value="">Please Select Investor</option>
-                                    @foreach ( $users as $user ) 
-                                        <option value="{{$user->ID}}" data-first="{{$user->FIRST_NAME}}" data-Last="{{$user->LAST_NAME}}">{{$user->FIRST_NAME.' '.$user->LAST_NAME}}</option>
-                                    @endforeach
+                                    <?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?> 
+                                        <option value="<?php echo e($user->ID); ?>" data-first="<?php echo e($user->FIRST_NAME); ?>" data-Last="<?php echo e($user->LAST_NAME); ?>"><?php echo e($user->FIRST_NAME.' '.$user->LAST_NAME); ?></option>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                 </optgroup>
                             </select>
                         </div>     
@@ -35,27 +34,27 @@
                     <div class="col-lg-6">
                         <div class="form-flex-item-box">
                             <label for="First Name">First Name</label>
-                            <input type="text" class="form-control" name="FIRST_NAME" value="{{$user->FIRST_NAME}}" id="FIRST_NAME" readonly>
+                            <input type="text" class="form-control" name="FIRST_NAME" value="<?php echo e($user->FIRST_NAME); ?>" id="FIRST_NAME" readonly>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="form-flex-item-box">
                             <label for="Last Name">Last Name</label>
-                            <input type="text" class="form-control" name="LAST_NAME" value="{{$user->LAST_NAME}}" id="LASTT_NAME" readonly>
+                            <input type="text" class="form-control" name="LAST_NAME" value="<?php echo e($user->LAST_NAME); ?>" id="LASTT_NAME" readonly>
                         </div>
                     </div>
        
                     <div class="col-lg-6">
                         <div class="form-flex-item-box">
                             <label for="Total Share">Total Share</label>
-                            <input type="number" class="form-control" name="TOTAL_SHARE" id="TOTAL_SHARE">
+                            <input type="text" class="form-control" name="TOTAL_SHARE" id="TOTAL_SHARE">
                         </div>
                     </div>
                 
                     <div class="col-lg-6">
                         <div class="form-flex-item-box">
                             <label for="Cash">Cash</label>
-                            <input type="number" class="form-control" name="CASH" id="CASH">
+                            <input type="text" class="form-control" name="CASH" id="CASH">
                         </div>
                     </div>
                     <div class="col-lg-6">
@@ -92,7 +91,7 @@
            </div>  
                 <div class="form-content pt-0">
                     <div class="form-btn-flex-between">
-                        <button type="button" class="form-cancel-btn"><a href="{{url()->previous()}}" style="color:black">Cancel</a></button>
+                        <button type="button" class="form-cancel-btn"><a href="<?php echo e(url()->previous()); ?>" style="color:black">Cancel</a></button>
                         <button type="submit" class="form-submit-btn">Submit</button>
                     </div>
                 </div>
@@ -102,4 +101,5 @@
  
 <!--// Main Area End //-->
 
-@endsection
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('backend.inc.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/epic-portal-php/resources/views/backend/template/entity-shareholding/entity-newpartner-add.blade.php ENDPATH**/ ?>
